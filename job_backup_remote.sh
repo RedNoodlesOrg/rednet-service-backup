27c4df4d674ffaea7e49f7a8983f7a2d47a53d99
Sam Imboden <70846253+FakeApate@users.noreply.github.com>
2024-12-04 18:45:57 +0000

    WIP

--- job_backup_remote.sh
+++ job_backup_remote.sh
@@ -1,13 +1,9 @@
 #!/usr/bin/env sh
 
 source="/vm/data-root/volumes"
-dest="onedrive:"
+dest="onedrive:docker_backup/"
 move_old_files_to="dated_directory"
 options="--filter-from=/rclone_jobber/filter_rules --checksum --onedrive-token=${ONEDRIVE_TOKEN} --onedrive-drive-id=${ONEDRIVE_DRIVE_ID} --onedrive-drive-type ${ONEDRIVE_DRIVE_TYPE}"
-monitoring_URL=""
+monitoring_URL="${MONITORING_URL}"
 
 /rclone_jobber/rclone_jobber.sh "$source" "$dest" "$move_old_files_to" "$options" "$(basename $0)" "$monitoring_URL"
-
-#display test directories (display for tutorial only, don't do this in production)
-tree -a $source
-rclone ls $dest
