866da4f9667ecda0a22b037a00c4101c1309e372
Sam Imboden <70846253+FakeApate@users.noreply.github.com>
2024-12-04 20:09:13 +0000

    WIP

--- job_backup_remote.sh
+++ job_backup_remote.sh
@@ -3,7 +3,7 @@
 source="/vm/data-root/volumes"
 dest="onedrive:docker_backup"
 move_old_files_to="dated_directory"
-options="--filter-from=/rclone_jobber/filter_rules --checksum --onedrive-token=${ONEDRIVE_TOKEN} --onedrive-drive-id=${ONEDRIVE_DRIVE_ID} --onedrive-drive-type=${ONEDRIVE_DRIVE_TYPE}"
+options="--filter-from=/rclone_jobber/filter_rules --checksum"
 monitoring_URL="${MONITORING_URL}"
 
 /rclone_jobber/rclone_jobber.sh "$source" "$dest" "$move_old_files_to" "$options" "$(basename $0)" "$monitoring_URL"
